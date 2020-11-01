22466205b8cba596e8bf49a8b4912240cd3cea1d
DESKTOP-JUGA8VD\123 <u10614227@ms.ttu.edu.tw>
2020-11-01 23:18:31 +0800

    整合:Floating 11/1 23:18

---
+++ Assets/Scripts/FloatPhysic.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatPhysic : MonoBehaviour
+{
+    [SerializeField]float SurfaceHeight;
+    [SerializeField]float OriginHeight;
+    public float noiseScale;
+    float floatSpeed;
+    public GameObject Water;
+    Rigidbody2D rb;
+    private float GForce;
+    private float Buoyancy;
+    public float FloatParameter;
+    [SerializeField]private float RisistForce;
+    public float WaterRisistForce;
+    public float MixRisistForce;
+    [Range(0.0f,1.0f)]public float density;
+    [SerializeField]private float OwnSizeHeight;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        SurfaceHeight = Water.transform.GetChild(0).transform.position.y;
+        OriginHeight = SurfaceHeight;
+        rb = GetComponent<Rigidbody2D>();
+        OwnSizeHeight = GetComponent<Collider2D>().bounds.size.y;
+    }
+    void Start()
+    {
+        GForce = OwnSizeHeight*density*FloatParameter;
+    }
+    void FixedUpdate()
+    {
+        rb.AddForce(Vector2.down*GForce);
+        rb.AddForce(Vector2.up*Buoyancy);
+    }
+    void Update()
+    {
+        BuoyancyChange();
+        AddRisistance();
+        // print("GForce"+GForce);
+        // print("Buoyancy"+Buoyancy);
+        Wave();
+        
+        rb.velocity = new Vector2(-2.0f,rb.velocity.y);
+    }
+    
+    void BuoyancyChange()
+    {
+        float SinkDist = SurfaceHeight-(transform.position.y-OwnSizeHeight/2);
+        
+        Buoyancy = Mathf.Clamp(SinkDist,0.0f,OwnSizeHeight)*FloatParameter;
+        
+    }
+    void AddRisistance()
+    {
+        float SinkDist = SurfaceHeight-(transform.position.y-OwnSizeHeight/2);
+
+        if(SinkDist>0)
+        {
+            if(rb.velocity.y<0)
+            {
+                rb.AddForce(Vector2.up*RisistForce);
+            }
+            else
+            {
+                rb.AddForce(Vector2.down*RisistForce);
+            }
+        }
+
+        if(SinkDist<OwnSizeHeight && SinkDist>0)
+        {
+            RisistForce = WaterRisistForce*(SinkDist/OwnSizeHeight);
+        }
+        else
+        {
+            RisistForce = WaterRisistForce;
+        }
+        
+    }
+    void Wave()
+    {
+        SurfaceHeight = OriginHeight + noiseScale * (Mathf.PerlinNoise(Time.time,0.0f)-0.5f);
+    }
+}
