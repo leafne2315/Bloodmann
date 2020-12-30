0c3510ac06909220a45b6752c9dd9017e23de540
leafne2315 <nekocat40305@gmail.com>
2020-12-31 04:38:08 +0800

    no message

---
+++ Assets/Scripts/FlyingBug.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingBug : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Circle Dance")]
+    public float DanceSpeed;
+    private Vector3 DanceDir;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+    }
+}
