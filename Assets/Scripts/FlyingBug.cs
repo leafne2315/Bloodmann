f6ffeeb98f828cbc98dc00bd787b8528ec6c9b62
DESKTOP-JUGA8VD\123 <u10614227@ms.ttu.edu.tw>
2020-12-31 21:10:45 +0800

    no message

--- Assets/Scripts/FlyingBug.cs
+++ Assets/Scripts/FlyingBug.cs
@@ -6,17 +6,34 @@ public class FlyingBug : MonoBehaviour
 {
     // Start is called before the first frame update
     [Header("Circle Dance")]
-    public float DanceSpeed;
+    [SerializeField]private float DanceSpeed ;
     private Vector3 DanceDir;
-
+    public float CycleTime;
+    public float radius;
+    public Transform CenterPos;
+    private Vector3 CentDir;
+    private Vector3 currentDir;
+    private Vector3 currentVel;
+    private Rigidbody rb;
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        currentDir = Vector3.right;
+        radius = Vector3.Distance(transform.position,CenterPos.position);
         
+        DanceSpeed = radius*2*Mathf.PI/CycleTime;
+        currentVel = currentDir*DanceSpeed;
+        print(currentVel);
     }
 
     // Update is called once per frame
     void Update()
     {
-            
+        rb.velocity = currentVel;
+        
+        CentDir = (CenterPos.position-transform.position);
+        currentVel = 2*Mathf.PI/CycleTime*CentDir;
+        print(currentVel);
+        
     }
 }
