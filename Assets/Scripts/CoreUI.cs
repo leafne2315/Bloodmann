4bd9c059cb8e1769d606f333d0c3cf0566146c95
ukciigh <cccap52@gmail.com>
2021-02-22 17:55:56 +0800

    UI

---
+++ Assets/Scripts/CoreUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CoreUI : MonoBehaviour
+{
+    private PlayerCtroller playerCtrScript;
+    public GameObject Player;
+    public Image[] cores;
+    public Sprite fullCore;
+    public Sprite emptyCore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCtrScript = Player.GetComponent<PlayerCtroller>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i=0; i<cores.Length; i++)
+        {
+            if(i<playerCtrScript.AttackRemain)
+            {
+                cores[i].sprite = fullCore;
+            }
+            else
+            {
+                cores[i].sprite = emptyCore;
+            }
+            
+            if(i<playerCtrScript.FullRemain)
+            {
+                cores[i].enabled = true;
+            }
+            else
+            {
+                cores[i].enabled = false;
+            }
+        }
+    }
+}
