3af99a5c96999af4bc83f145d15e7c5224779181
DESKTOP-JUGA8VD\123 <u10614227@ms.ttu.edu.tw>
2021-05-03 22:19:11 +0800

    light control & bug dying collider

---
+++ Assets/Scripts/LightShowRange.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightShowRange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] Lights;
+    public float showingRange;
+    public GameObject Player;
+    void Start()
+    {
+        InvokeRepeating("checkLight",0.0f,0.3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void checkLight()
+    {
+        for(int i =0;i<Lights.Length;i++)
+        {
+            if(Vector3.Distance(Player.transform.position,Lights[i].transform.position)<showingRange)
+            {
+                Lights[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                Lights[i].gameObject.SetActive(false);
+            }
+        }
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(Player.transform.position,showingRange);
+    }
+}
